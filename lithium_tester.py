865da9157ca8957f82f1440d193223939ca9e297
Gabi <abrahams.gabi@gmail.com>
2018-10-06 12:26:17 +1000

    stuff

--- lithium_tester.py
+++ lithium_tester.py
@@ -12,19 +12,22 @@ def signal_handler(signal, frame):
 signal.signal(signal.SIGINT, signal_handler)
 interrupted = False
 
-s = serial.Serial('COM1')
+s = serial.Serial('ttyUSB0')
 li = pylithium.Lithium(s)
 
-transmitting = False
+transmitting = True
 sent = []
 if transmitting:
+    filename = input("Enter savefile name: ")
+    input("Press any key to start transmitting...")
     for packet_size in [48, 96, 128, 255]:
         for i in range(1, 101):
             packet = bytes([i].append([random.randint(0, 2**8-1) for i in range(packet_size-1)]))
             sent.append(packet)
             li.transmit(packet)
             time.sleep(0.5)
-
+    with open(filename+".pkl","wb") as f:
+        pickle.dump(sent, f) 
 else:
     received = []
     while True:
